230af4a2a43bc9531c49a64ef5f2aee03c4dd881
Eugene <mahdi@panto.org>
2025-08-13 10:27:15 +0330

    :bricks: Add Exception filter

---
+++ ChatApp.Api/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,51 @@
+using System.Net;
+using System.Text.Json;
+using ChatApp.Application.Exceptions;
+
+namespace ChatApp.Api.Middleware;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var response = new
+        {
+            message = "An unexpected error occurred.",
+            errors = new Dictionary<string, string[]>()
+        };
+
+        if (exception is ValidationException validationException)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            response = new
+            {
+                message = "One or more validation errors occurred.",
+                errors = new Dictionary<string, string[]>(validationException.Errors)
+            };
+        }
+
+        return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+    }
+}
